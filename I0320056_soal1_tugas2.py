470ac9ac2d216adc2c31d0514a670624c038b30a
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-12 00:00:03 +0700

    Membuat soal 1

--- I0320056_soal1_tugas2.py
+++ I0320056_soal1_tugas2.py
@@ -5,3 +5,46 @@ l = float(input("masukkan lebar: "))
 
 luas_persegipanjang = p * l
 print("\nluas persegi panjang: ", luas_persegipanjang)
+
+
+
+print("\nMenghitung Luas Lingkaran")
+
+r = float(input("masukkan jari-jari: "))
+
+luas_lingkaran = 3.14 * (r ** 2)
+print("\nluas lingkaran: ", luas_lingkaran)
+
+
+
+print("\nMenghitung Luas Kubus")
+
+r = float(input("masukkan nilai rusuk: "))
+
+luas_kubus = 6 * (r ** 2)
+print("\nluas kubus: ", luas_kubus)
+
+
+
+print("\nMenghitung Konversi Suhu(Celcius ke Fahrenheit)")
+
+C = float(input("masukkan suhu(celcius): "))
+
+F = 9/5 * C + 32
+print("\nCelcius: ", C)
+print("\nFahrenheit: ", F)
+
+
+
+print("\nMenghitung Konversi Suhu(Reamur ke Kelvin)")
+
+R = float(input("masukkan suhu(reamur): "))
+
+K = 5/4 * R + 273
+print("\nReamur: ", R)
+print("\nKelvin: ", K)
+
+
+
+
+
