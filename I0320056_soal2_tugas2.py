4f892506f3f58edd1adae627ec6aced31391080d
MariaLaveniaVikaPamukasari <vikalavenia@gmail.com>
2021-03-12 18:12:50 +0700

    Mengerjakan soal nomor 2

---
+++ I0320056_soal2_tugas2.py
@@ -0,0 +1,69 @@
+print("Biodata")
+
+Nama = ("Maria Lavenia Vika Pamukasari")
+print("\nNama lengkap saya", Nama)
+Sapaan = ("Vika")
+print("Saya biasa disapa", Sapaan)
+NIM = ("I0320056")
+print("NIM saya adalah", NIM)
+Kelas = ("B")
+print("Saat ini saya duduk di kelas", Kelas)
+Semester = 2
+print("Semester", Semester)
+Prodi = ("Teknik Industri")
+print("Program Studi", Prodi)
+Sekolah = ("Universitas Sebelas Maret Surakarta")
+print(Sekolah)
+Asal = ("Sragen, Jawa Tengah")
+print("Saya berasal dari", Asal)
+Alamat = ("RT/RW 02/01, Patihan, Sidoharjo, Sragen")
+print("Saya tinggal di", Alamat)
+Hari = 15
+Bulan = 9
+Tahun = 2002
+print("Saya lahir pada", Hari, Bulan, Tahun)
+Umur1 = 18
+Umur2 = 5
+Umur3 = 27
+print("Umur saya", Umur1, "tahun", Umur2, "bulan", Umur3, "hari")
+Tinggi_badan = 154
+print("Tinggi badan saya adalah", Tinggi_badan)
+Berat_badan = 55.4
+print("Berat badan saya adalah", Berat_badan)
+Sepatu = 39
+print("Saya memiliki ukuran sepatu", Sepatu)
+Hobi = ("menyanyi")
+print("Saya memiliki hobi", Hobi)
+Cita_cita = ("manager")
+print("Saya ingin menjadi seorang", Cita_cita)
+Warna = ("coklat")
+print("Warna favorit saya adalah", Warna)
+Makanan = ("klepon")
+print("Makanan favorit saya adalah", Makanan)
+Minuman = ("susu")
+print("minuman favorit saya adalah", Minuman)
+Jarak = 37.6
+print("Jarak rumah saya ke kampus adalah", Jarak)
+Matkul = ("Kalkulus")
+print("Saya menyukai mata kuliah", Matkul)
+Bahasa = ("Indonesia,Inggris,dan Jerman")
+print("Saya menguasasi bahasa", Bahasa)
+Film = ("Horor")
+print("Saya menyukai film", Film)
+Hal_suka = ("dihargai")
+print("Hal yang saya sukai adalah", Hal_suka)
+Hal_benci = ("ditipu")
+print("Hal yang tidak saya sukai adalah", Hal_benci)
+Lagu = ("Superheroes")
+print("Lagu favorit saya adalah", Lagu)
+Organisasi = ("BEM FT,PIKR UNS,SRE UNS,YOT SOLO")
+print("Saat ini saya tergabung dalam", Organisasi)
+Idola = ("Putri Tanjung")
+print("Tokoh idola saya adalah", Idola)
+Hewan = ("Anjing")
+print("Hewan kesukaan saya adalah", Hewan)
+Bunga = ("Mawar")
+print("Bunga kesukaan saya adalah", Bunga)
+Tempat = ("pantai")
+print("Tempat favorit saya adalah", Tempat)
+
